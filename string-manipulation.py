e3b1ae6fe0069f1bbe74b794ca113b683960ac80
Enis Hamza <eniss.hamza@gmail.com>
2026-03-25 15:46:31 +0100

    End string manipulation task

---
+++ string-manipulation.py
@@ -0,0 +1,4 @@
+text = "Data engineering is powerful and data pipelines are important"
+# Counting how many times we use data word in variable and replacing word data with DATA
+print(text.count("data"))
+print(text.replace("data", "DATA"))

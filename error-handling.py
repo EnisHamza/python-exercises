1a54ec393fd2bbfbff30995f89e3b10723122f8c
Enis Hamza <eniss.hamza@gmail.com>
2026-03-25 15:31:01 +0100

    End withdrawing function with error handling

--- error-handling.py
+++ error-handling.py
@@ -1,2 +1,15 @@
 def withdraw(balance, amount):
-    pass
+    # Withdrawing amount from the balance and handling errors without crashing the app
+    try:
+        if amount > balance:
+            raise ValueError("Amount can not be grater than balance!")
+        if amount < 0:
+            raise ValueError("Invalid amount!")
+        new_balance = balance - amount
+        return f"Withdrawal successful. Your current balance is: {new_balance}"
+    except ValueError as error:
+        print("Error: ", error)
+        return f"Your current balance is: {balance}"
+
+
+print(withdraw(30, 20))

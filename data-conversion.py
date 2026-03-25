a0c798306fa2efbd72746efbcc7af37e3774e604
Enis Hamza <eniss.hamza@gmail.com>
2026-03-25 14:29:58 +0100

    Convert string values to int

---
+++ data-conversion.py
@@ -0,0 +1,12 @@
+def convert_to_string(values):
+    valid_values = []
+    for value in values:
+        try:
+            valid_values.append(int(value))
+        except (ValueError, TypeError):
+            continue
+    return valid_values
+
+
+values = ["10", "20", "abc", "40"]
+print(convert_to_string(values))
